fc0e53a6a4ba2123dae9e25365dae3c29c7452ea
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2023-06-03 10:23:07 +0100

    Removed old code and new under-development code

--- PutridParrot.Delimited.Data/Tools/DelimitedStreamBuilder.cs
+++ PutridParrot.Delimited.Data/Tools/DelimitedStreamBuilder.cs
@@ -1,86 +1,86 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
 
-namespace PutridParrot.Delimited.Data.Tools
-{
-#if DEBUG // needs more testing before it can get release
-    public class DelimitedStreamBuilder
-    {
-        private readonly List<DelimitedStreamReader> _readers;
+//namespace PutridParrot.Delimited.Data.Tools
+//{
+//#if DEBUG // needs more testing before it can get release
+//    public class DelimitedStreamBuilder
+//    {
+//        private readonly List<DelimitedStreamReader> _readers;
 
-        private readonly List<Func<IList<string>, bool>> _filters;
-        private readonly List<Func<IList<string>, IList<string>>> _processors;
+//        private readonly List<Func<IList<string>, bool>> _filters;
+//        private readonly List<Func<IList<string>, IList<string>>> _processors;
 
-        private DelimitedStreamBuilder()
-        {
-            _readers = new List<DelimitedStreamReader>();
-            _filters = new List<Func<IList<string>, bool>>();
-            _processors = new List<Func<IList<string>, IList<string>>>();
-        }
+//        private DelimitedStreamBuilder()
+//        {
+//            _readers = new List<DelimitedStreamReader>();
+//            _filters = new List<Func<IList<string>, bool>>();
+//            _processors = new List<Func<IList<string>, IList<string>>>();
+//        }
 
-        public static DelimitedStreamBuilder New() =>
-            new();
+//        public static DelimitedStreamBuilder New() =>
+//            new();
 
-        public static DelimitedStreamBuilder New(DelimitedStreamReader reader)
-        {
-            if (reader == null)
-                throw new ArgumentNullException(nameof(reader));
+//        public static DelimitedStreamBuilder New(DelimitedStreamReader reader)
+//        {
+//            if (reader == null)
+//                throw new ArgumentNullException(nameof(reader));
 
-            var instance = New();
-            instance.Concat(reader);
-            return instance;
-        }
+//            var instance = New();
+//            instance.Concat(reader);
+//            return instance;
+//        }
 
-        public DelimitedStreamBuilder Concat(DelimitedStreamReader reader)
-        {
-            if (reader == null)
-                throw new ArgumentNullException(nameof(reader));
+//        public DelimitedStreamBuilder Concat(DelimitedStreamReader reader)
+//        {
+//            if (reader == null)
+//                throw new ArgumentNullException(nameof(reader));
             
-            _readers.Add(reader);
-            return this;
-        }
+//            _readers.Add(reader);
+//            return this;
+//        }
 
-        public DelimitedStreamBuilder Select(Func<IList<string>, IList<string>> processor)
-        {
-            _processors.Add(processor);
-            return this;
-        }
+//        public DelimitedStreamBuilder Select(Func<IList<string>, IList<string>> processor)
+//        {
+//            _processors.Add(processor);
+//            return this;
+//        }
 
-        public DelimitedStreamBuilder Where(Func<IList<string>, bool> filter)
-        {
-            _filters.Add(filter);
-            return this;
-        }
+//        public DelimitedStreamBuilder Where(Func<IList<string>, bool> filter)
+//        {
+//            _filters.Add(filter);
+//            return this;
+//        }
 
-        private bool Filter(IList<string> fields) =>
-            _filters.Any(f => f(fields));
+//        private bool Filter(IList<string> fields) =>
+//            _filters.Any(f => f(fields));
 
-        private IList<string> Process(IList<string> fields) =>
-            _processors.Aggregate(fields, (current, processor) => processor(current));
+//        private IList<string> Process(IList<string> fields) =>
+//            _processors.Aggregate(fields, (current, processor) => processor(current));
 
-        public void Write(DelimitedStreamWriter? writer, int skip = 0)
-        {
-            if(writer == null)
-                throw new ArgumentNullException(nameof(writer));
+//        public void Write(DelimitedStreamWriter? writer, int skip = 0)
+//        {
+//            if(writer == null)
+//                throw new ArgumentNullException(nameof(writer));
 
-            foreach (var reader in _readers)
-            {
-                for (var i = 0; i < skip; i++)
-                {
-                    reader.ReadLine();
-                }
+//            foreach (var reader in _readers)
+//            {
+//                for (var i = 0; i < skip; i++)
+//                {
+//                    reader.ReadLine();
+//                }
 
-                while (reader.ReadLine() is { } fields)
-                {
-                    if (!Filter(fields))
-                    {
-                        writer.WriteLine(Process(fields));
-                    }
-                }
-            }
-            writer.Flush();
-        }
-    }
-#endif
-}
+//                while (reader.ReadLine() is { } fields)
+//                {
+//                    if (!Filter(fields))
+//                    {
+//                        writer.WriteLine(Process(fields));
+//                    }
+//                }
+//            }
+//            writer.Flush();
+//        }
+//    }
+//#endif
+//}
--- Tests.PutridParrot.Delimited.Data/DelimitedStreamBuilderTests.cs
+++ Tests.PutridParrot.Delimited.Data/DelimitedStreamBuilderTests.cs
@@ -1,86 +1,84 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
-using System.IO;
-using NUnit.Framework;
-using PutridParrot.Delimited.Data.Specializations;
-using PutridParrot.Delimited.Data.Tools;
+﻿//using System;
+//using System.Diagnostics.CodeAnalysis;
+//using System.IO;
+//using NUnit.Framework;
+//using PutridParrot.Delimited.Data.Specializations;
+//using PutridParrot.Delimited.Data.Tools;
 
-namespace Tests.PutridParrot.Delimited.Data
-{
-#if DEBUG // needs more testing before it can get release
-	[ExcludeFromCodeCoverage]
-	public class DelimitedStreamBuilderTests
-	{
-		class MyStream : MemoryStream
-		{
-			// we want to stop the serializer automatically disposing of the stream
-			public override void Close()
-			{
-			}
+//namespace Tests.PutridParrot.Delimited.Data
+//{
+//	[ExcludeFromCodeCoverage]
+//	public class DelimitedStreamBuilderTests
+//	{
+//		class MyStream : MemoryStream
+//		{
+//			// we want to stop the serializer automatically disposing of the stream
+//			public override void Close()
+//			{
+//			}
 
-			public void ForceClose()
-			{
-				base.Close();
-			}
-		}
+//			public void ForceClose()
+//			{
+//				base.Close();
+//			}
+//		}
 
-		[Test]
-		public void Connect_WithNullInput_NoExceptionsExpectedAndNoOutput()
-		{
-			var ms = new MyStream();
-			var writer = new TsvWriter(ms);
+//		[Test]
+//		public void Connect_WithNullInput_NoExceptionsExpectedAndNoOutput()
+//		{
+//			var ms = new MyStream();
+//			var writer = new TsvWriter(ms);
 
-			DelimitedStreamBuilder.New().Write(writer);
+//			DelimitedStreamBuilder.New().Write(writer);
 
-			Assert.AreEqual(0, ms.Length);
-		}
+//			Assert.AreEqual(0, ms.Length);
+//		}
 
-		[Test]
-		public void Connect_WithNullOutput()
-		{
-			var reader = new CsvReader(Utils.ToStream("Hello, World"));
-			Assert.Throws<ArgumentNullException>(() => DelimitedStreamBuilder.New(reader).Write(null));
-		}
+//		[Test]
+//		public void Connect_WithNullOutput()
+//		{
+//			var reader = new CsvReader(Utils.ToStream("Hello, World"));
+//			Assert.Throws<ArgumentNullException>(() => DelimitedStreamBuilder.New(reader).Write(null));
+//		}
 
-		[Test]
-		public void Connect_TurnCsvIntoTsv()
-		{
-			var ms = new MyStream();
+//		[Test]
+//		public void Connect_TurnCsvIntoTsv()
+//		{
+//			var ms = new MyStream();
 
-			var reader = new CsvReader(Utils.ToStream("Hello, World"));
-			var writer = new TsvWriter(ms);
+//			var reader = new CsvReader(Utils.ToStream("Hello, World"));
+//			var writer = new TsvWriter(ms);
 
-            DelimitedStreamBuilder.New(reader).Write(writer);
+//            DelimitedStreamBuilder.New(reader).Write(writer);
 
-			ms.Position = 0;
+//			ms.Position = 0;
 
-			var sr = new StreamReader(ms);
-			var result = sr.ReadToEnd();
+//			var sr = new StreamReader(ms);
+//			var result = sr.ReadToEnd();
 
-			ms.ForceClose();
+//			ms.ForceClose();
 
-			Assert.AreEqual($"Hello\tWorld{Environment.NewLine}", result);
-		}
+//			Assert.AreEqual($"Hello\tWorld{Environment.NewLine}", result);
+//		}
 
-		[Test]
-		public void Connect_TurnCsvIntoTsvSkipFirstThreeLines()
-		{
-			var ms = new MyStream();
+//		[Test]
+//		public void Connect_TurnCsvIntoTsvSkipFirstThreeLines()
+//		{
+//			var ms = new MyStream();
 
-			var reader = new CsvReader(Utils.ToStream("one\r\ntwo\r\nthree\r\nHello, World"));
-			var writer = new TsvWriter(ms);
+//			var reader = new CsvReader(Utils.ToStream("one\r\ntwo\r\nthree\r\nHello, World"));
+//			var writer = new TsvWriter(ms);
 
-            DelimitedStreamBuilder.New(reader).Write(writer, 3);
+//            DelimitedStreamBuilder.New(reader).Write(writer, 3);
 
-			ms.Position = 0;
+//			ms.Position = 0;
 
-			var sr = new StreamReader(ms);
-			var result = sr.ReadToEnd();
+//			var sr = new StreamReader(ms);
+//			var result = sr.ReadToEnd();
 
-			ms.ForceClose();
+//			ms.ForceClose();
 
-			Assert.AreEqual($"Hello\tWorld{Environment.NewLine}", result);
-		}
-	}
-#endif
-}
+//			Assert.AreEqual($"Hello\tWorld{Environment.NewLine}", result);
+//		}
+//	}
+//}
